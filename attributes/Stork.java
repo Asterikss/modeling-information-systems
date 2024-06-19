3b8a65e59cfd3f7dfb52556ab5be46333c7864c1
Andre <AndreAndreM@proton.me>
2024-06-19 23:23:30 +0200

    added stork to attributes/

---
+++ attributes/Stork.java
@@ -0,0 +1,113 @@
+import java.io.Serializable;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Stork implements Serializable {
+
+    private static int staticId = 0;
+    // Ekstensja klasy
+    private static List<Stork> storkList = new ArrayList<>();
+
+    private int id;
+    private boolean isKnown;
+    // Atrybut opcjonalny
+    private String name;
+
+    // Atrybut powtarzalny i wymagany
+    private Set<CardinalDirection> cameFrom = new HashSet<>();
+    
+    public Stork(String name, CardinalDirection dir) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+        this.isKnown = true;
+        this.name = name;
+        this.cameFrom.add(dir);
+        this.id = staticId;
+        staticId++;
+        storkList.add(this);
+    }
+
+    // Przeciążenie metody / konstruktora
+    public Stork(CardinalDirection dir) {
+        this.isKnown = false;
+        this.cameFrom.add(dir);
+        this.id = staticId;
+        staticId++;
+        storkList.add(this);
+    }
+
+    public static void setStorkList(List<Stork> newStorkList) {
+        storkList = new ArrayList<Stork>(newStorkList);
+    }
+
+    public void setName(String name) {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        if (name != null) {
+            this.isKnown = true;
+        }
+        this.name = name;
+    }
+
+    public boolean isKnown() {
+        return this.isKnown;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void addOrigin(CardinalDirection dir) {
+        this.cameFrom.add(dir);
+    }
+    
+    public void removeOrigin(CardinalDirection dir) {
+        if (!this.cameFrom.contains(dir)) {
+            throw new IllegalArgumentException("This cardinal direction is not present in the set");
+        }
+        if (this.cameFrom.size() <= 1) {
+            throw new IllegalArgumentException("Cannot remove the last cardinal direction from the set");
+        }
+        this.cameFrom.remove(dir);
+    }
+
+    // Metoda klasowa
+    public static List<Stork> getStorkList() {
+        return Collections.unmodifiableList(storkList);
+    }
+
+    // Atrybut pochodny i metoda klasowa
+    public static int getStorksFromDirection(CardinalDirection dir) {
+        int count = 0;
+        for (Stork stork : storkList) {
+            if (stork.cameFrom.contains(dir)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void setStaticId(int newStaticId) {
+        staticId = newStaticId;
+    }
+
+    public static int getStaticId() {
+        return staticId;
+    }
+
+    // Przesłonięcie metody
+    public String toString() {
+        return "Stork{id= " + id + ", name= " + name + "}";
+    }
+
+}
