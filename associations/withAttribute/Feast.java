b9889972f1efa44e5639a3eda368183c8c11b43e
Andre <AndreAndreM@proton.me>
2024-07-03 23:53:55 +0200

    added Feast to withAttribute example

---
+++ associations/withAttribute/Feast.java
@@ -0,0 +1,57 @@
+package withAttribute;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Feast {
+    private String name;
+
+    private Set<Participation> participations = new HashSet<>();
+
+    public Feast(String name) {
+        setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+        this.name = name;
+    }
+
+    public Set<Participation> getParticipations() {
+        return new HashSet<>(participations);
+    }
+
+    public void addParticipation(Participation p) {
+        if (p == null) {
+            throw new IllegalArgumentException("Participation cannot be null");
+        }
+        if (p.getFeast() != this) {
+            throw new IllegalArgumentException("Participation cannot be reassigned");
+        }
+        if (!participations.contains(p)) {
+            participations.add(p);
+        }
+
+    }
+
+    public void removeParticipation(Participation p) {
+        if (p == null) {
+            throw new IllegalArgumentException("Participation cannot be null");
+        }
+        if (!participations.contains(p)) {
+            throw new IllegalArgumentException("Cannot remove a participation that is not in the participations list");
+
+        }
+        participations.remove(p);
+        if (p.getCook() != null && p.getCook().getParticipations().contains(p)) {
+            p.getCook().removeParticipation(p);
+        }
+
+    }
+}
