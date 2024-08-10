df914d5934784f04dfdc394514f2c76cb807d281
Andre <AndreAndreM@proton.me>
2024-08-10 23:58:01 +0200

    added Cook to withAttribute example

---
+++ constraints/withAttribute/Cook.java
@@ -0,0 +1,49 @@
+package withAttribute;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Cook {
+    private String name;
+
+    private Set<Participation> participations = new HashSet<>();
+
+    public Cook(String name) {
+        setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+        this.name = name;
+    }
+
+    public Set<Participation> getParticipations() {
+        return new HashSet<>(participations);
+    }
+
+    public void addParticipation(Participation p) {
+        if (p == null) {
+           throw new IllegalArgumentException("Participation cannot be null");
+        }
+        if (p.getCook() != this) {
+            throw new IllegalArgumentException("Participation cannot be reassigned");
+        }
+        if (!participations.contains(p)) {
+            participations.add(p);
+        }
+
+    }
+
+    public void removeParticipation(Participation p) {
+        participations.remove(p);
+        if (p.getFeast() != null && p.getFeast().getParticipations().contains(p)) {
+            p.getFeast().removeParticipation(p);
+        }
+    }
+}
